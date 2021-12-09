a88b3193fc8d485633e450cce707ef7f2d14226f
MihailTanasoglo <heterszeus@gmail.com>
2021-12-09 20:13:54 +0200

    add tests

---
+++ CarAPI.Tests/EdgeDriverTest.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace CarAPI.Tests
+{
+    [TestClass]
+    public class EdgeDriverTest
+    {
+
+
+        private EdgeDriver _driver;
+
+        [TestInitialize]
+        public void EdgeDriverInitialize()
+        {
+            var options = new EdgeOptions
+            {
+                PageLoadStrategy = PageLoadStrategy.Normal
+            };
+            _driver = new EdgeDriver(options);
+        }
+
+        [TestMethod]
+        public void VerifyPageTitle()
+        {
+            // Replace with your own test logic
+            _driver.Url = "https://www.bing.com";
+            Assert.AreEqual("Bing", _driver.Title);
+        }
+
+        [TestCleanup]
+        public void EdgeDriverCleanup()
+        {
+            _driver.Quit();
+        }
+    }
+}
