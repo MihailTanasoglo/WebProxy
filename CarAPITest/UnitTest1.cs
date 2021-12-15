6c140672c7ebbf61d4fbf974084318a265242647
MihailTanasoglo <heterszeus@gmail.com>
2021-12-15 20:36:46 +0200

    New Tests

---
+++ CarAPITest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace CarAPITest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(4, 2+2);
+
+        }
+    }
+}
